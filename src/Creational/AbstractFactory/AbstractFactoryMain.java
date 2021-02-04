d2e0d0714347642a9eefe5d68f261aebb0f6ed66
Horki <ivanzvonimirhorvat@gmail.com>
2021-02-05 00:07:25 +0100

    abstract factory; fix typo

--- src/Creational/AbstractFactory/AbstractFactoryMain.java
+++ src/Creational/AbstractFactory/AbstractFactoryMain.java
@@ -1,12 +1,11 @@
 package Creational.AbstractFactory;
 
-
 import Creational.AbstractFactory.Factory.Abstract.PizzaStore;
 import Creational.AbstractFactory.Factory.Concrete.ChicagoPizzaStore;
 import Creational.AbstractFactory.Factory.Concrete.NYPizzaStore;
 import Creational.AbstractFactory.Product.Abstract.Pizza;
 
-public class FactoryMethodMain {
+public class AbstractFactoryMain {
     public static void main(String[] args) {
         PizzaStore nyStore = new NYPizzaStore();
         PizzaStore chicagoStore = new ChicagoPizzaStore();
